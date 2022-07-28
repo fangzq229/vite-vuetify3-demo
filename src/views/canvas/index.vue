f396d7852a104de03f8db5dcc1146d3c97d44777
fangzhenqi <m18811126599@163.com>
2022-07-28 17:28:28 +0800

    test

--- src/views/canvas/index.vue
+++ src/views/canvas/index.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
 import { nextTick, reactive, ref } from "@vue/runtime-core";
+import img1 from "@/assets/image/1.png";
+import img2 from "@/assets/image/2.png";
 export default {
   setup() {
     // 起始坐标
@@ -16,36 +18,73 @@ export default {
     nextTick(async () => {
       const canvas = <HTMLCanvasElement>document.getElementById("canvas")!;
       const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
-      const xNumber = 8; // x 轴网格数量
-      const yNumber = 6; // y 轴网格数量
-      canvas.width = 6400;
-      canvas.height = 4800;
-      canvas.style.width = "6400px";
-      canvas.style.height = "4800px";
-      const block = 800; // 网格大小
+      const xNumber = 50; // x 轴网格数量
+      const yNumber = 50; // y 轴网格数量
+      canvas.width = 500;
+      canvas.height = 500;
+      canvas.style.width = "500px";
+      canvas.style.height = "500px";
+      const block = 10; // 网格大小
       ctx.fillStyle = "#f9f9f9";
-      ctx.strokeStyle = "#f1f1f1"; // 边框的颜色
-      const imgObj = new Image();
-      imgObj.src =
-        "https://nft-1259661131.cos.ap-hongkong.myqcloud.com/test/fang-test/map-hk.jpeg";
-      imgObj.onload = async function () {
-        for (let i = 0; i < xNumber; i++) {
-          for (let j = 0; j < yNumber; j++) {
-            const mapBit = await createImageBitmap(
-              imgObj,
-              i * block,
-              j * block,
-              block,
-              block
-            );
-            ctx.fillRect(i * block, j * block, block, block);
-            // 绘制边框;
-            ctx.strokeRect(i * block, j * block, block, block);
-            ctx.drawImage(mapBit, i * block, j * block);
-            console.log("----" + i, j);
-          }
+      // ctx.strokeStyle = "#f1f1f1"; // 边框的颜色
+      // const imgObj = new Image();
+      // imgObj.src =
+      //   "https://nft-1259661131.cos.ap-hongkong.myqcloud.com/test/fang-test/map-hk.jpeg";
+      // imgObj.onload = async function () {
+      // for (let i = 0; i < xNumber; i++) {
+      //   for (let j = 0; j < yNumber; j++) {
+      //     // const mapBit = await createImageBitmap(
+      //     //   imgObj,
+      //     //   i * block,
+      //     //   j * block,
+      //     //   block,
+      //     //   block
+      //     // );
+      //     ctx.fillRect(i * block, j * block, block, block);
+      //     // 绘制边框;
+      //     // ctx.strokeRect(i * block, j * block, block, block);
+      //     // ctx.drawImage(mapBit, i * block, j * block);
+      //     console.log("----" + i, j);
+      //   }
+      // }
+      // }
+
+      darw("#F1f1f1");
+
+      // 画图
+      function darw(color: string = "#f9f9f9") {
+        ctx.fillStyle = color;
+        ctx.strokeStyle = "#f1f1f1"; // 边框的颜色
+        const imgObj = new Image();
+        if (zoom.value > 2) {
+          imgObj.src = img2;
+        } else {
+          imgObj.src = img1;
         }
-      };
+        imgObj.onload = async function () {
+          ctx.drawImage(imgObj, 0, 0);
+          for (let i = 0; i < xNumber; i++) {
+            for (let j = 0; j < yNumber; j++) {
+              // const mapBit = await createImageBitmap(
+              //   imgObj,
+              //   i * block,
+              //   j * block,
+              //   block,
+              //   block
+              // );
+              if (checkXy(i * block + 10, j * block, mousedownXy)) {
+                ctx.fillStyle = "red";
+                ctx.fillRect(i * block, j * block, block, block);
+              } else {
+                // ctx.fillStyle = color;
+              }
+              // ctx.fillRect(i * block, j * block, block, block);
+              // 绘制边框;
+              // ctx.strokeRect(i * block, j * block, block, block);
+            }
+          }
+        };
+      }
 
       // 键盘事件
       document.addEventListener("keydown", (res: KeyboardEvent) => {
@@ -73,14 +112,15 @@ export default {
           x: mousedownXy[mousedownXy.length - 1].x,
           y: mousedownXy[mousedownXy.length - 1].y,
         };
-
+        // 移动坐标是否以存在
+        const isInclude = mousedownXy.find((resXy) => {
+          return mXy.x == resXy.x && mXy.y == resXy.y;
+        });
         if (mXy.x == mousedownXy[0].x && mXy.y == mousedownXy[0].y) {
           isStart.value = false;
-        } else if (
-          mousedownXy.length > 1 &&
-          mXy.x == mousedownXy[mousedownXy.length - 2].x &&
-          mXy.y == mousedownXy[mousedownXy.length - 2].y
-        ) {
+          // 闭环结束 计算可用坐标
+          darw("#F1f1f1");
+        } else if (isInclude) {
           return false;
         } else {
           mousedownXy.push(mXy);
@@ -95,6 +135,42 @@ export default {
         ctx.lineTo(mXy.x, mXy.y);
         ctx.stroke();
       });
+
+      // 计算圈地坐标
+      function checkXy(x: number, y: number, poly: { x: number; y: number }[]) {
+        let iSum = 0;
+        let iCount = 0;
+        let dLon1, dLon2, dLat1, dLat2, dLon;
+        if (poly.length < 3) {
+          return false;
+        }
+        iCount = poly.length;
+        for (let i = 0; i < iCount; i++) {
+          if (i == iCount - 1) {
+            dLon1 = poly[i].x;
+            dLat1 = poly[i].y;
+            dLon2 = poly[0].x;
+            dLat2 = poly[0].y;
+          } else {
+            dLon1 = poly[i].x;
+            dLat1 = poly[i].y;
+            dLon2 = poly[i + 1].x;
+            dLat2 = poly[i + 1].y;
+          }
+          //以下语句判断A点是否在边的两端点的水平平行线之间，在则可能有交点，开始判断交点是否在左射线上
+          if ((y >= dLat1 && y < dLat2) || (y >= dLat2 && y < dLat1)) {
+            if (Math.abs(dLat1 - dLat2) > 0) {
+              //得到 A点向左射线与边的交点的x坐标：
+              dLon = dLon1 - ((dLon1 - dLon2) * (dLat1 - y)) / (dLat1 - dLat2);
+              if (dLon < x) iSum++;
+            }
+          }
+        }
+        if (iSum % 2 != 0) {
+          return true;
+        }
+        return false;
+      }
 
       // 鼠标按下弹起事件
       canvas.addEventListener("mouseup", (res: MouseEvent) => {
@@ -147,14 +223,15 @@ export default {
       // 放大缩小
       canvas.addEventListener("mousewheel", (e: any) => {
         if (e.wheelDeltaY < 0 && zoom.value < 3) {
-          zoom.value = Number((zoom.value + 0.01).toFixed(2));
+          zoom.value = Number((zoom.value + 0.1).toFixed(2));
           canvas.style.transform = "scale(" + zoom.value + ")";
-        } else if (e.wheelDeltaY > 0 && zoom.value > 0.3) {
-          zoom.value = Number((zoom.value - 0.01).toFixed(2));
+        } else if (e.wheelDeltaY > 0 && zoom.value > 1) {
+          zoom.value = Number((zoom.value - 0.1).toFixed(2));
           canvas.style.transform = "scale(" + zoom.value + ")";
         } else {
           return;
         }
+        darw("green");
       });
     });
     return {};
